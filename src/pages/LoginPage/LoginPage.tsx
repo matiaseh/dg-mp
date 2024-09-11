2f95c5af95887346eefe770af811a63d5fac4e9b
Matias Hynnä <hynna.matias@gmail.com>
2024-09-11 16:50:35 +0300

    Redirect back home from /login if user has authenticated

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
 import './LoginPage.css';
 import AlertComponent from '../../components/AlertComponent';
+import { useAuth } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const [errorMessage, updateErrorMessage] = useState<string | null>(null);
+
+  // Redirect to home if already authenticated
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/');
+    }
+  }, [isAuthenticated, navigate]);
 
   const handleFlip = () => {
     setShowRegisterForm(!showRegisterForm);
