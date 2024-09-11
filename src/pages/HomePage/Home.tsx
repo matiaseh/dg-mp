99d6312a5f21a37400c46f2c3c21d593b206f3a0
Matias Hynnä <hynna.matias@gmail.com>
2024-09-11 16:59:40 +0300

    Add temporary home and profile page titles

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useOutletContext } from 'react-router-dom';
 import PostsList from './components/PostsList';
+import { Text } from '@chakra-ui/react';
 
 export interface LayoutContext {
   refreshKey: number;
@@ -9,7 +10,12 @@ export interface LayoutContext {
 const Home: React.FC = () => {
   const { refreshKey } = useOutletContext<LayoutContext>();
 
-  return <PostsList refreshKey={refreshKey} />;
+  return (
+    <div>
+      <Text>Home</Text>
+      <PostsList refreshKey={refreshKey} />
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/HomePage/Profile.tsx
+++ src/pages/HomePage/Profile.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { useOutletContext } from 'react-router-dom';
 import { LayoutContext } from './Home';
 import PostsList from './components/PostsList';
+import { Text } from '@chakra-ui/react';
 
 const Profile: React.FC = () => {
   const { refreshKey } = useOutletContext<LayoutContext>();
 
-  return <PostsList showOwnPosts={true} refreshKey={refreshKey} />;
+  return (
+    <div>
+      <Text>My profile</Text>
+      <PostsList showOwnPosts={true} refreshKey={refreshKey} />
+    </div>
+  );
 };
 
 export default Profile;
